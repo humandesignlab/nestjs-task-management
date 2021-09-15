cbc6a195866bca8bbec1a7be0ae782ef28abed00
Humberto Mangino <humberto.mangino@dollarshaveclub.com>
2021-09-15 09:40:24 -0600

    Adds configuration

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -6,15 +6,17 @@ import { UsersRepository } from './users.repository';
 
 import { User } from './user.entity';
 import { JwtPayload } from './jwt-payload.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
 	constructor(
 		@InjectRepository(UsersRepository)
 		private usersRepository: UsersRepository,
+		private configService: ConfigService,
 	) {
 		super({
-			secretOrKey: 'topSecret51',
+			secretOrKey: configService.get('JWT_SECRET'),
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 		});
 	}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,14 +10,13 @@ import {
 	UseGuards,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './task.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '../auth/user.entity';
-import { GetUser } from 'src/get-user.decorator';
+import { GetUser } from '../get-user.decorator';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
